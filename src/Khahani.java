f968b68297ee545a440a4d791677d41bb815d0be
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 06:09:15 -0700

    How to use common operators in java

---
+++ src/Khahani.java
@@ -0,0 +1,29 @@
+/**
+ * Developer and Teacher: Khahani.com
+ * Course: Android 1
+ */
+
+public class Khahani {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        System.out.println(a + 1);
+        System.out.println(a - 2);
+        System.out.println(a * 3);
+        System.out.println(a / 4);
+        System.out.println(a % 2);
+
+        a = a + 1;
+        a++;
+        a--;
+
+        a = a + 3;
+        a+=3;
+        a-=3;
+        a*=3;
+        a/=3;
+        a%=3;
+
+    }
+}
